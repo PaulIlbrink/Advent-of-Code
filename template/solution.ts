d1b4eb812f1237364aec2cb2361e8188cba7116a
Paul Ilbrink <paul@script.nl>
2024-12-15 21:15:19 +0100

    .

--- template/solution.ts
+++ template/solution.ts
@@ -7,7 +7,7 @@ export function solve(input: string): SolveResult {
 
   return {
     description: `Part is ${chalk.underline.white(
-      "not solved yet zaad"
+      "not solved yet"
     )}, and part 2 is ${chalk.underline.yellow("not solved yet")}.`,
   };
 }
