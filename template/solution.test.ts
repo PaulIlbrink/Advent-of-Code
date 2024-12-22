e612cb0028a2f9047e589a37d2128c1297de697c
Paul Ilbrink <paul@script.nl>
2024-12-22 16:44:01 +0100

    Some template updates after day 7

--- template/solution.test.ts
+++ template/solution.test.ts
@@ -1,25 +1,34 @@
-import { beforeAll, describe, expect, it } from "bun:test";
+import { beforeAll, describe, expect, it, test } from "bun:test";
 import { readFileSync } from "fs";
-import { solve } from "./solution";
+import { resetState, solve } from "./solution";
 import { resolve } from "path";
 
 let exampleInput: string;
 
 beforeAll(() => {
   exampleInput = readFileSync(resolve(__dirname, "input.example.txt"), "utf-8");
+  resetState();
+});
+
+describe(`Day ${__dirname} functions`, () => {
+  test.skip("some day specific function", () => {
+    const foo = null;
+
+    expect(foo).toBeTrue();
+  });
 });
 
 describe(`Day ${__dirname} example`, () => {
-  it("Part 1 example", () => {
+  test("Part 1 example", () => {
     const { part1 } = solve(exampleInput);
 
-    expect(part1).toBeUndefined();
+    expect(part1).toBe(123);
   });
 
-  it("Part 2 example", () => {
+  test.skip("Part 2 example", () => {
     const { part2 } = solve(exampleInput);
 
-    expect(part2).toBeUndefined();
+    expect(part2).toBe(123);
   });
 
   // Add more test cases if needed
--- template/solution.ts
+++ template/solution.ts
@@ -1,9 +1,12 @@
 import chalk from "chalk";
 
-const state = {};
+export type State = {
+  // prop: []
+};
+const state: State = {};
 
-const resetState = () => {
-  // state.clear()
+export const resetState = () => {
+  // state.prop = [];
 };
 
 const parseInput = (input: string): void => {
@@ -21,16 +24,16 @@ export function solve(input: string): SolveResult {
 
   /* --------------------------------- Part 1 --------------------------------- */
 
-  const part1 = "not solved yet";
+  const part1: number = 0; // not solved yet
   const part1fmt = chalk.underline.white(part1);
   let description = `Part 1 result is ${part1fmt}`;
 
   /* --------------------------------- Part 2 --------------------------------- */
 
-  const part2 = "not solved yet";
+  const part2: number = 0; // not solved yet
   const part2fmt = chalk.underline.yellow(part2);
   description += `, and part 2 is ${part2fmt}.`;
 
   /* --------------------------------- Result --------------------------------- */
-  return { description };
+  return { description, part1, part2 };
 }
