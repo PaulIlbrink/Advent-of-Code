5791b1ddc6be57c0573a284d33fea3c1559a078a
Paul Ilbrink <paul@script.nl>
2025-01-04 03:23:32 +0100

    Day 13

--- 2024/13/solution.test.ts
+++ 2024/13/solution.test.ts
@@ -14,27 +14,31 @@ describe(`Day ${__dirname} functions`, () => {
   test("parseInput", () => {
     expect(() => parseInput(exampleInput)).not.toThrowError();
 
-    expect(() => parseInput("foo bar")).toThrowError(/^Invalid input/);
+    expect(() => parseInput("foo bar")).toThrowError(/^Missing input/);
+
+    expect(() => parseInput("foo\nbar\nlorem\nipsum")).toThrowError(
+      /^Invalid input/
+    );
 
     const incompleteInput = exampleInput.split("\n").slice(0, 5).join("\n");
     expect(exampleInput).toStartWith(incompleteInput);
     expect(exampleInput).not.toEqual(incompleteInput);
-    
+
     expect(() => parseInput(incompleteInput)).toThrowError(/^Incomplete input/); // doesn't work
   });
 });
 
 describe(`Day ${__dirname} example`, () => {
-  test.skip("Part 1 example", () => {
+  test("Part 1 example", () => {
     const { part1 } = solve(exampleInput);
 
-    expect(part1).toBe(123);
+    expect(part1).toBe(480);
   });
 
-  test.skip("Part 2 example", () => {
+  test("Part 2 example", () => {
     const { part2 } = solve(exampleInput);
 
-    expect(part2).toBe(123);
+    expect(part2).toBe(875318608908);
   });
 
   // Add more test cases if needed
--- 2024/13/solution.ts
+++ 2024/13/solution.ts
@@ -9,6 +9,11 @@ export type Machine = {
   a: Button;
   b: Button;
   prize: Coordinate;
+  strategy?: Strategy;
+};
+export type Strategy = {
+  a: number;
+  b: number;
 };
 
 export enum INPUT_STATE {
@@ -23,6 +28,11 @@ export type State = {
 export const state: State = {
   machines: [],
 };
+
+export enum TOKENS {
+  BTN_A = 3,
+  BTN_B = 1,
+}
 
 export const resetState = () => {
   state.machines.length = 0;
@@ -42,22 +52,29 @@ export const parseInput = (input: string): void => {
   const machineGroups: Record<string, string>[] = [];
 
   const lines = input.split("\n").map((line) => line.trim());
+  let matches: RegExpExecArray | null;
+
   if (lines.length < inputPatterns.length)
-    throw new Error("Invalid input, not enough lines for a single machine");
+    throw new Error("Missing input, not enough lines for a single machine");
 
   // parse the machines
   lines.forEach((line, i) => {
     // ignore empty line
     if (line === "") return;
 
+    // execute the regex
+    matches = inputPatterns[inputState].exec(line);
+
     // invalid input
-    if (!inputPatterns[inputState].test(line))
+    if (matches === null || !matches.groups)
       throw new Error(
-        `Input "${line}" doesn't match the expected machine pattern ${inputPatterns[inputState]}`
+        `Invalid input, "${line}" is not a valid ${
+          inputState === INPUT_STATE.PRIZE ? "prize" : "button"
+        }.`
       );
 
     // valid input, push the naming groups into an array
-    machineGroups.push(inputPatterns[inputState].exec(line)!.groups!);
+    machineGroups.push(matches.groups);
 
     // be ready for next input
     inputState = (inputState + 1) % 3;
@@ -82,23 +99,75 @@ export const parseInput = (input: string): void => {
   // somehow doesn't get here
   if (machineGroups.length > 0)
     throw new Error(
-      `Incomplete input, can't make a make machine with ${machineGroups}`
+      `Incomplete input, only ${machineGroups.length} lines found for the final machine.`
     );
+};
+
+// find a winning strategy f
+export const findStrategy = (
+  machine: Machine,
+  prizePositionCorrection: number = 0
+): void => {
+  const {
+    a: { x: Ax, y: Ay },
+    b: { x: Bx, y: By },
+    prize: { x: Px, y: Py },
+  } = machine;
+
+  const PxC = Px + prizePositionCorrection;
+  const PyC = Py + prizePositionCorrection;
+
+  const As = (By * PxC - Bx * PyC) / (By * Ax - Bx * Ay);
+  const Bs = (Ay * PxC - Ax * PyC) / (Ay * Bx - Ax * By);
+
+  machine.strategy = {
+    a: As,
+    b: Bs,
+  };
+};
+
+export const solveMachines = (prizePositionCorrection: number = 0) => {
+  const { machines } = state;
+  machines.forEach((m) => findStrategy(m, prizePositionCorrection));
+};
+
+export const calculateTokens = (): number => {
+  const { machines } = state;
+
+  return machines.reduce((tokens, machine) => {
+    // no solution
+    if (!machine.strategy)
+      throw new Error(`No strategy for machine ${machine}`);
+
+    const { a, b } = machine.strategy;
+
+    // fractions, there's no such thing as half button presses
+    if (!Number.isInteger(a) || !Number.isInteger(b)) return tokens;
+
+    tokens += a * TOKENS.BTN_A + b * TOKENS.BTN_B;
+
+    return tokens;
+  }, 0);
 };
 
 export function solve(input: string): SolveResult {
   /* ---------------------------------- Setup --------------------------------- */
   parseInput(input);
 
+  solveMachines();
+
   /* --------------------------------- Part 1 --------------------------------- */
-  const part1: number = 0; // not solved yet
+  const part1: number = calculateTokens(); // not solved yet
   const part1fmt = chalk.underline.white(part1);
-  let description = `Part 1 result is ${part1fmt}`;
+  let description = `To win all possible prizes you need to spend ${part1fmt} tokens`;
 
   /* --------------------------------- Part 2 --------------------------------- */
-  const part2: number = 0; // not solved yet
+
+  solveMachines(10000000000000);
+
+  const part2: number = calculateTokens(); // not solved yet
   const part2fmt = chalk.underline.yellow(part2);
-  description += `, and part 2 is ${part2fmt}.`;
+  description += `, and after correcting the conversion mistake you need to spend ${part2fmt} tokens.`;
 
   /* --------------------------------- Result --------------------------------- */
   return { description, part1, part2 };
