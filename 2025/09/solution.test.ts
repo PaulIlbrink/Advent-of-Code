884d57c9d39837fbc84912736ee8cb7103b09ed4
Paul Ilbrink <138305525+PaulIlbrink@users.noreply.github.com>
2025-12-10 04:33:18 +0100

    Lets grab some sleep

--- 2025/09/solution.test.ts
+++ 2025/09/solution.test.ts
@@ -1,14 +1,18 @@
 import { beforeAll, beforeEach, describe, expect, test } from "bun:test";
-import { readFileSync } from "fs";
+import { Dir, readFileSync } from "fs";
 import {
-  isFullColor,
+  isColorRectangle,
   parseInput,
   resetState,
   solve,
   rectSize,
   state,
+  setDirection,
+  type Tile,
+  rightTurns,
 } from "./solution";
 import path, { resolve } from "path";
+import { Direction } from "../../2024/06/solution";
 
 let exampleInput: string;
 const dayNumber = path.basename(__dirname);
@@ -18,55 +22,60 @@ beforeAll(() => {
   resetState();
 });
 
-describe(`Day ${dayNumber} input`, () => {
-  beforeEach(() => {
-    parseInput(exampleInput);
-  });
-
-  test("state props", () => {
-    let { redTiles, rowColors, colColors } = state;
-
-    expect(redTiles).toBeArrayOfSize(8);
-
-    expect(redTiles[3]).toEqual([9, 7]);
-
-    expect(colColors).toHaveLength(4);
-
-    const colKeys = colColors.keys().toArray();
-    expect(colKeys).toBeArrayOfSize(4);
-    expect(new Set(colKeys)).toEqual(new Set([2, 7, 9, 11]));
-
-    const rowKeys = rowColors.keys().toArray();
-    expect(rowKeys).toBeArrayOfSize(4);
-    expect(new Set(rowKeys)).toEqual(new Set([1, 3, 5, 7]));
-
-    expect(colColors.has(0)).toBeFalse();
-    expect(colColors.has(2)).toBeTrue();
-    expect(colColors.get(2)).toEqual([[3, 5]]);
-    expect(colColors.get(4)).toBeUndefined();
-
-    expect(rowColors).toHaveLength(4);
-    expect(rowColors.has(0)).toBeFalse();
-    expect(rowColors.has(1)).toBeTrue();
-    expect(rowColors.get(1)).toEqual([[7, 11]]);
-    expect(rowColors.get(3)).toEqual([[2, 7]]);
-    expect(rowColors.get(4)).toBeUndefined();
-
-    // expand color ranges
-    expect(colColors.get(7)).toEqual([[1, 3]]);
-    expect(rowColors.get(3)).toEqual([[2, 7]]);
-
-    // expandColors();
-    // colColors = state.colColors;
-    // rowColors = state.rowColors;
-    // expect(colColors.get(7)).not.toEqual([[1, 3]]);
-    // expect(colColors.get(7)).toEqual([[1, 5]]);
-    // expect(rowColors.get(3)).not.toEqual([[2, 7]]);
-    // expect(rowColors.get(3)).toEqual([[2, 11]]);
-  });
-});
-
 describe(`Day ${dayNumber} functions`, () => {
+  test("rightTurns", () => {
+    let prevDir: Direction | undefined;
+
+    expect(rightTurns(Direction.N, prevDir)).toBe(0);
+    expect(rightTurns(Direction.E, prevDir)).toBe(0);
+    expect(rightTurns(Direction.S, prevDir)).toBe(0);
+    expect(rightTurns(Direction.W, prevDir)).toBe(0);
+
+    prevDir = Direction.N;
+    expect(rightTurns(Direction.N, prevDir)).toBe(0);
+    expect(rightTurns(Direction.E, prevDir)).toBe(1);
+    expect(rightTurns(Direction.S, prevDir)).toBe(2);
+    expect(rightTurns(Direction.W, prevDir)).toBe(-1);
+
+    prevDir = Direction.E;
+    expect(rightTurns(Direction.N, prevDir)).toBe(-1);
+    expect(rightTurns(Direction.E, prevDir)).toBe(0);
+    expect(rightTurns(Direction.S, prevDir)).toBe(1);
+    expect(rightTurns(Direction.W, prevDir)).toBe(2);
+
+    prevDir = Direction.S;
+    expect(rightTurns(Direction.N, prevDir)).toBe(2);
+    expect(rightTurns(Direction.E, prevDir)).toBe(-1);
+    expect(rightTurns(Direction.S, prevDir)).toBe(0);
+    expect(rightTurns(Direction.W, prevDir)).toBe(1);
+
+    prevDir = Direction.W;
+    expect(rightTurns(Direction.N, prevDir)).toBe(1);
+    expect(rightTurns(Direction.E, prevDir)).toBe(2);
+    expect(rightTurns(Direction.S, prevDir)).toBe(-1);
+    expect(rightTurns(Direction.W, prevDir)).toBe(0);
+  });
+
+  test("setDirection", () => {
+    let tile: Tile = [5, 5];
+    let tileNext: Tile = [5, 10];
+    let tilePrev: Tile = [0, 5, Direction.E];
+
+    let [_x, _y, tileDir] = tile;
+
+    expect(tileDir).toBeUndefined();
+
+    let rightTurns = setDirection(tile, tileNext);
+    [_x, _y, tileDir] = tile;
+
+    expect(tileDir).toBeDefined();
+    expect(tileDir).toBe(Direction.N);
+    expect(rightTurns).toBe(0);
+
+    rightTurns = setDirection(tile, tileNext, tilePrev);
+    expect(rightTurns).toBe(-1);
+  });
+
   test("squareSize", () => {
     expect(rectSize([1, 1], [1, 1])).toBe(1);
     expect(rectSize([1, 1], [2, 2])).toBe(4);
@@ -76,15 +85,135 @@ describe(`Day ${dayNumber} functions`, () => {
 
     expect(rectSize([2, 2], [22, 2])).toBe(21);
   });
+});
 
-  test("isFullColor", () => {
+describe(`Day ${dayNumber} input`, () => {
+  test("turnOffset", () => {
+    resetState();
+
+    let { turnOffset } = state;
+
+    expect(turnOffset).toBe(0);
+
     parseInput(exampleInput);
 
-    expect(isFullColor([7, 1], [11, 7])).toBeFalse();
+    ({ turnOffset } = state);
 
-    // expect(isFullColor([2, 3], [9, 5])).toBeFalse();
-    // expandColors();
-    // expect(isFullColor([2, 3], [9, 5])).toBeTrue();
+    expect(turnOffset).toBe(-4);
+
+    const rightPathInput = "0,0\n0,5\n5,5\n5,0";
+
+    parseInput(rightPathInput);
+    ({ turnOffset } = state);
+    expect(turnOffset).toBe(4);
+
+    const lineInput = "0,0\n10,0";
+    parseInput(lineInput);
+    ({ turnOffset } = state);
+    expect(turnOffset).toBe(4);
+
+    const altLineInput = "0,0\n-10,0";
+    parseInput(altLineInput);
+    ({ turnOffset } = state);
+    expect(turnOffset).toBe(4);
+  });
+
+  test("redTiles", () => {
+    parseInput(exampleInput);
+
+    const { redTiles } = state;
+
+    expect(redTiles).toBeArrayOfSize(8);
+
+    const tileAt3 = redTiles.at(3);
+    expect(tileAt3).toBeDefined();
+
+    let [x, y, dir] = tileAt3!;
+    expect([x, y]).toEqual([9, 7]);
+    expect(dir).toBeDefined();
+    expect(dir).toBe(Direction.S);
+    expect([x, y, dir]).toEqual([9, 7, Direction.S]);
+
+    [x, y, dir] = redTiles.at(0)!;
+    expect([x, y]).toEqual([7, 1]);
+    expect(dir).toBeDefined();
+    expect(dir).toBe(Direction.E);
+
+    [x, y, dir] = redTiles.at(1)!;
+    expect([x, y]).toEqual([11, 1]);
+    expect(dir).toBeDefined();
+    expect(dir).toBe(Direction.N);
+
+    [x, y, dir] = redTiles.at(2)!;
+    expect([x, y]).toEqual([11, 7]);
+    expect(dir).toBeDefined();
+    expect(dir).toBe(Direction.W);
+
+    [x, y, dir] = redTiles.at(7)!;
+    expect([x, y]).toEqual([7, 3]);
+    expect(dir).toBeDefined();
+    expect(dir).toBe(Direction.S);
+
+    expect(redTiles.at(99)).toBeUndefined();
+
+    const missingDirections = redTiles.some(
+      ([x, y, direction]) => direction === undefined
+    );
+    expect(missingDirections).toBeFalse();
+  });
+
+  test("edges", () => {
+    resetState();
+
+    const { colEdges, rowEdges } = state;
+
+    expect(colEdges).toBeEmpty();
+    expect(rowEdges).toBeEmpty();
+
+    parseInput(exampleInput);
+
+    expect(colEdges).not.toBeEmpty();
+    expect(rowEdges).not.toBeEmpty();
+
+    expect(colEdges).toHaveLength(4);
+    expect(rowEdges).toHaveLength(4);
+
+    const colKeys = colEdges.keys();
+    expect(new Set(colKeys)).toEqual(new Set([2, 7, 9, 11]));
+
+    expect(colEdges.has(0)).toBeFalse();
+
+    expect(colEdges.has(2)).toBeTrue();
+    const colEdge2 = colEdges.get(2);
+    expect(colEdge2).toBeArrayOfSize(1);
+    expect(colEdge2?.at(0)).toBeArray();
+    expect(colEdge2?.at(0)).toEqual([5, 3]);
+    expect(colEdge2?.at(0)).not.toEqual([3, 5]);
+
+    expect(colEdges.get(4)).toBeUndefined();
+
+    const rowKeys = rowEdges.keys();
+    expect(new Set(rowKeys)).toEqual(new Set([1, 3, 5, 7]));
+
+    expect(rowEdges).toHaveLength(4);
+    expect(rowEdges.has(0)).toBeFalse();
+
+    expect(rowEdges.has(1)).toBeTrue();
+    expect(rowEdges.get(1)?.at(0)).toEqual([7, 11]);
+    expect(rowEdges.get(1)?.at(0)).not.toEqual([11, 7]);
+
+    expect(rowEdges.get(3)?.at(0)).toEqual([2, 7]);
+    expect(rowEdges.get(3)?.at(0)).not.toEqual([7, 2]);
+
+    expect(rowEdges.get(4)).toBeUndefined();
+  });
+
+  describe("input based functions", () => {
+    test("isFullColor", () => {
+      parseInput(exampleInput);
+
+      expect(isColorRectangle([1, 1], [1, 1])).toBeTrue();
+    });
   });
 });
 
@@ -95,7 +224,7 @@ describe(`Day ${dayNumber} example`, () => {
     expect(part1).toBe(50);
   });
 
-  test.skip("Part 2 example", () => {
+  test("Part 2 example", () => {
     const { part2 } = solve(exampleInput);
 
     expect(part2).toBe(24);
--- 2025/09/solution.ts
+++ 2025/09/solution.ts
@@ -1,187 +1,242 @@
 import chalk from "chalk";
-import type { Coordinate } from "../../2024/06/solution";
+import { Direction, type Coordinate } from "../../2024/06/solution";
 
-export type ColorRange = [start: number, end: number];
+export type Edge = [start: number, end: number];
 
-export type ColorMap = Map<number, ColorRange[]>;
+export type EdgeMap = Map<number, Edge[]>;
+
+export type Tile = [x: number, y: number, direction?: Direction];
 
 export type State = {
-  redTiles: Coordinate[];
-  colColors: ColorMap;
-  rowColors: ColorMap;
+  redTiles: Tile[];
+  colEdges: EdgeMap;
+  rowEdges: EdgeMap;
+  turnOffset: number;
+  clockWise: boolean;
 };
 export const state: State = {
   redTiles: [],
-  colColors: new Map(),
-  rowColors: new Map(),
+  turnOffset: 0,
+  colEdges: new Map(),
+  rowEdges: new Map(),
+  clockWise: false,
 };
 
 export const resetState = () => {
   state.redTiles.length = 0;
-  state.colColors.clear();
-  state.rowColors.clear();
+  state.turnOffset = 0;
+  state.colEdges.clear();
+  state.rowEdges.clear();
+  state.clockWise = false;
 };
 
 export const addColorRange = (
   [xA, yA]: Coordinate,
   [xB, yB]: Coordinate
 ): void => {
-  const { colColors, rowColors } = state;
+  //   const {} = state;
+  //   if (xA !== xB && yA !== yB) throw new Error("Coordinates are not alligned!");
+  //   if (xA === xB) {
+  //     if (!colColors.has(xA)) colColors.set(xA, []);
+  //     colColors.get(xA)!.push([Math.min(yA, yB), Math.max(yA, yB)]);
+  //     return;
+  //   }
+  //   if (!rowColors.has(yA)) rowColors.set(yA, []);
+  //   rowColors.get(yA)!.push([Math.min(xA, xB), Math.max(xA, xB)]);
+};
 
-  if (xA !== xB && yA !== yB) throw new Error("Coordinates are not alligned!");
+export const rightTurns = (
+  direction: Direction,
+  prevDirection?: Direction
+): number => {
+  if (!prevDirection) return 0;
 
-  if (xA === xB) {
-    if (!colColors.has(xA)) colColors.set(xA, []);
+  const change = (4 + direction - prevDirection) % 4;
 
-    colColors.get(xA)!.push([Math.min(yA, yB), Math.max(yA, yB)]);
+  return ((change + 1) % 4) - 1;
+};
+
+export const addEdge = (idx: number, edge: Edge, isRow = false): void => {
+  const [start, end] = edge;
+  if (start === end)
+    throw new Error(
+      `We shouldn't have a zero-length edge? [idx, [start,end], isRow] = [${idx}, [${start}, ${end}], ${isRow}]`
+    );
+
+  const { colEdges, rowEdges } = state;
+  if (isRow) {
+    if (!rowEdges.has(idx)) rowEdges.set(idx, []);
+
+    rowEdges.get(idx)!.push(edge);
     return;
   }
 
-  if (!rowColors.has(yA)) rowColors.set(yA, []);
+  if (!colEdges.has(idx)) colEdges.set(idx, []);
 
-  rowColors.get(yA)!.push([Math.min(xA, xB), Math.max(xA, xB)]);
+  colEdges.get(idx)!.push(edge);
+};
+
+export const setDirection = (
+  tile: Tile,
+  nextTile: Tile,
+  prevTile?: Tile
+): number => {
+  const [x, y] = tile;
+  const [xNext, yNext] = nextTile;
+
+  if (x === xNext && y === yNext) throw new Error("Overlapping tiles");
+
+  let direction;
+  if (x === xNext) {
+    direction = yNext > y ? Direction.N : Direction.S;
+    tile[2] = direction;
+
+    if (!prevTile) return 0;
+
+    addEdge(x, [y, yNext]);
+
+    const [_x, _y, prevDirection] = prevTile;
+    return rightTurns(direction, prevDirection);
+  }
+
+  // y === yNext
+  direction = xNext > x ? Direction.E : Direction.W;
+  tile[2] = direction;
+
+  if (!prevTile) return 0;
+
+  const [_x, _y, prevDirection] = prevTile;
+  addEdge(y, [x, xNext], true);
+
+  return rightTurns(direction, prevDirection);
 };
 
 export const parseInput = (input: string): void => {
+  if (!input) throw new Error("Input is missing");
+
   resetState();
+
   const lines = input.split("\n").map((line) => line.trim());
 
-  const { redTiles, colColors, rowColors } = state;
+  const { redTiles } = state;
 
-  let prevCoord = lines[lines.length - 1].split(",").map(Number) as Coordinate;
+  let prevTile, prevPrevTile;
+  let turnOffset = 0;
+
   for (const line of lines) {
     const numbers = line.split(",").map(Number);
     if (numbers.length !== 2) continue;
 
-    const coord = numbers as Coordinate;
+    const tile = numbers as Tile;
 
-    addColorRange(prevCoord, coord);
+    if (prevTile) {
+      turnOffset += setDirection(prevTile, tile, prevPrevTile);
+      prevPrevTile = prevTile;
+    }
 
-    redTiles.push(coord);
-
-    prevCoord = coord;
+    redTiles.push(tile);
+    prevTile = tile;
   }
+
+  if (redTiles.length < 2)
+    throw new Error("The input is incomplete, at least 2 red tiles are needed");
+
+  // the first 2 tiles need to be revisited to complete the direction/turn values
+  for (const tile of redTiles.slice(0, 2)) {
+    turnOffset += setDirection(prevTile!, tile, prevPrevTile!);
+    prevPrevTile = prevTile;
+    prevTile = tile;
+  }
+
+  state.turnOffset = turnOffset;
+  state.clockWise = turnOffset === 4;
 };
 
-export const expandColors = (): void => {
-  return;
-
-  //   const { colColors, rowColors } = state;
-
-  //   const colIdxs = [...colColors.keys()].sort((a, b) => a - b);
-  //   const rowIdxs = [...rowColors.keys()].sort((a, b) => a - b);
-
-  //   const colIdxsInv = colIdxs.toReversed();
-  //   const rowIdxsInv = rowIdxs.toReversed();
-
-  //   const colColorsExp: ColorMap = new Map();
-  //   const rowColorsExp: ColorMap = new Map();
-
-  //   // expand columns
-  //   for (let [x, [yMin, yMax]] of colColors) {
-  //     // lower yMin
-  //     for (const y of rowIdxs) {
-  //       if (y >= yMin) break;
-
-  //       const [xMin, xMax] = rowColors.get(y)!;
-  //       // out of bounds
-  //       if (x < xMin || x > xMax) continue;
-
-  //       yMin = y;
-  //       break;
-  //     }
-
-  //     // increase yMax
-  //     for (const y of rowIdxsInv) {
-  //       if (y <= yMax) break;
-
-  //       const [xMin, xMax] = rowColors.get(y)!;
-  //       // out of bounds
-  //       if (x < xMin || x > xMax) continue;
-
-  //       yMax = y;
-  //       break;
-  //     }
-
-  //     colColorsExp.set(x, [yMin, yMax]);
-  //   }
-
-  //   // expand rows
-  //   for (let [y, [xMin, xMax]] of rowColors) {
-  //     // lower yMin
-  //     for (const x of colIdxs) {
-  //       if (x >= xMin) break;
-
-  //       const [yMin, yMax] = colColors.get(x)!;
-  //       // out of bounds
-  //       if (y < yMin || y > yMax) continue;
-
-  //       xMin = x;
-  //       break;
-  //     }
-
-  //     // increase yMax
-  //     for (const x of colIdxsInv) {
-  //       if (x <= xMax) break;
-
-  //       const [yMin, yMax] = colColors.get(x)!;
-
-  //       // out of bounds
-  //       if (y < yMin || y > yMax) {
-  //         continue;
-  //       }
-
-  //       xMax = x;
-  //       break;
-  //     }
-
-  //     rowColorsExp.set(y, [xMin, xMax]);
-  //   }
-
-  //   state.colColors = colColorsExp;
-  //   state.rowColors = rowColorsExp;
-};
-
-export const rectSize = (
-  [xA, yA]: Coordinate,
-  [xB, yB]: Coordinate
-): number => {
+export const rectSize = ([xA, yA]: Tile, [xB, yB]: Tile): number => {
   return (Math.abs(xA - xB) + 1) * (Math.abs(yA - yB) + 1);
 };
 
-export const isRangeCovered = (
-  [startA, endA]: [number, number],
-  [startB, endB]: [number, number]
+export const isSideColored = (
+  idx: number,
+  side: Edge,
+  isRow = false
 ): boolean => {
+  const { rowEdges, colEdges } = state;
+  const [start, end] = side;
+
+  const sideMap = isRow ? rowEdges : colEdges;
+
+  // crossing lines or just a 180 degree point i guess
+  if (!sideMap.has(idx)) return false;
+
+  const sideEdges: Edge[] = sideMap.get(idx)!;
+
+  //
+  const fullyCovered = sideEdges.some(
+    ([startE, endE]) =>
+      Math.min(startE, endE) <= Math.min(start, end) &&
+      Math.max(start, end) <= Math.max(startE, endE)
+  );
+  return fullyCovered;
+
   return false;
 };
 
-export const isFullColor = (
-  [xA, yA]: Coordinate,
-  [xB, yB]: Coordinate
+export const isColorRectangle = (
+  [xA, yA, dirA]: Tile,
+  [xB, yB, dirB]: Tile
 ): boolean => {
-  return false;
+  if (xA === xB && yA === yB) return true;
+
+  // a single line rectangle, the area is small, so unlikely to be largest, ignore for now
+  if (xA === xB || yA === yB) return false;
+
+  // ok, we should check all 4 sides
+  const horizontal: Edge = [xA, xB];
+  const vertical: Edge = [yA, yB];
+
+  const topIdx = Math.max(yA, yB);
+  const bottomIdx = Math.min(yA, yB);
+  const leftIdx = Math.min(xA, xB);
+  const rightIdx = Math.max(xA, xB);
+
+  const topOk = isSideColored(topIdx, horizontal, true);
+  const bottomOk = isSideColored(bottomIdx, horizontal, true);
+  const leftOk = isSideColored(leftIdx, vertical, true);
+  const rightOk = isSideColored(rightIdx, vertical, true);
+
+  const sidesOk = [topOk, rightOk, bottomOk, leftOk];
+
+  const totalOk = sidesOk.length;
+
+  return totalOk >= 2;
+
+  //   return false;
 };
 
 export const maxRectangle = (fullColor = false): number => {
   const { redTiles } = state;
 
   let max = -1;
+  let maxA, maxB;
   for (const tileA of redTiles) {
     for (const tileB of redTiles) {
-      // rectangle is too small, regardless of color
+      // we already found a larger rectangle OR tiles are the same
       const size = rectSize(tileA, tileB);
-      if (size <= max) continue;
+      if (size <= max || size === 1) continue;
 
       // has non-colored tiles, abort
-      if (fullColor && !isFullColor(tileA, tileB)) {
+      if (fullColor && !isColorRectangle(tileA, tileB)) {
         continue;
       }
 
       max = size;
+      maxA = tileA;
+      maxB = tileB;
     }
   }
 
+  if (fullColor) console.log("masSize", max, "for tiles", maxA, "and", maxB);
   return max;
 };
 
@@ -196,9 +251,7 @@ export function solve(input: string): SolveResult {
 
   /* --------------------------------- Part 2 --------------------------------- */
 
-  //   expandColors();
-
-  const part2: number = 0; // maxSquare(true); // solved, but wrong
+  const part2: number = maxRectangle(true);
   const part2fmt = chalk.underline.yellow(part2);
   description += `, the largest rectangle using red and green tiles is ${part2fmt}.`;
 
