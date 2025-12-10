3c14b7da9c6caa0bf110bb51d3f86de1f42cc805
Paul Ilbrink <138305525+PaulIlbrink@users.noreply.github.com>
2025-12-10 23:58:07 +0100

    Day 10 template

---
+++ 2025/10/solution.test.ts
@@ -0,0 +1,46 @@
+import { beforeAll, beforeEach, describe, expect, test } from "bun:test";
+import { readFileSync } from "fs";
+import { parseInput, resetState, solve, state } from "./solution";
+import path, { resolve } from "path";
+
+let exampleInput: string;
+const dayNumber = path.basename(__dirname);
+
+beforeAll(() => {
+  exampleInput = readFileSync(resolve(__dirname, "input.example.txt"), "utf-8");
+  resetState();
+});
+
+describe(`Day ${dayNumber} input`, () => {
+  beforeEach(() => {
+    parseInput(exampleInput);
+  });
+
+  test.skip("state props", () => {
+    const {} = state;
+
+    expect(true).toBe(false);
+  });
+});
+
+describe(`Day ${dayNumber} functions`, () => {
+  test.skip("some day specific function", () => {
+    const foo = null;
+
+    expect(foo).toBeTrue();
+  });
+});
+
+describe(`Day ${dayNumber} example`, () => {
+  test.skip("Part 1 example", () => {
+    const { part1 } = solve(exampleInput);
+
+    expect(part1).toBe(123);
+  });
+
+  test.skip("Part 2 example", () => {
+    const { part2 } = solve(exampleInput);
+
+    expect(part2).toBe(123);
+  });
+});
